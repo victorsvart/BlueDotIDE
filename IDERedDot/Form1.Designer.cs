e122dd3bf67dcc4a58ae67975e15d280a257a39f
victorsvart <victor.cscience@gmail.com>
2022-09-14 09:26:22 -0300

    Adicionar arquivos de projeto.

---
+++ IDERedDot/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace IDERedDot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tree = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.codeRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tree
+            // 
+            this.tree.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(88)))));
+            this.tree.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tree.ForeColor = System.Drawing.Color.Silver;
+            this.tree.Location = new System.Drawing.Point(0, 68);
+            this.tree.Margin = new System.Windows.Forms.Padding(2);
+            this.tree.Name = "tree";
+            this.tree.Size = new System.Drawing.Size(176, 516);
+            this.tree.StateImageList = this.imageList1;
+            this.tree.TabIndex = 1;
+            this.tree.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tree_NodeMouseDoubleClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "icon.png");
+            this.imageList1.Images.SetKeyName(1, "folder.png");
+            // 
+            // codeRichTextBox
+            // 
+            this.codeRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.codeRichTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(60)))), ((int)(((byte)(88)))));
+            this.codeRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.codeRichTextBox.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.codeRichTextBox.Location = new System.Drawing.Point(180, 65);
+            this.codeRichTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.codeRichTextBox.Name = "codeRichTextBox";
+            this.codeRichTextBox.Size = new System.Drawing.Size(866, 519);
+            this.codeRichTextBox.TabIndex = 2;
+            this.codeRichTextBox.Text = "";
+            this.codeRichTextBox.TextChanged += new System.EventHandler(this.codeRichTextBox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.BorderSize = 50;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(1, 42);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "File";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 42);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 42);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "View";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(182, 42);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "About";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(242, 42);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(56, 22);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Help";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFolderToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(140, 48);
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openFolderToolStripMenuItem.Text = "Open Folder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(1045, 587);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.codeRichTextBox);
+            this.Controls.Add(this.tree);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "BlueDot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Controls.SetChildIndex(this.tree, 0);
+            this.Controls.SetChildIndex(this.codeRichTextBox, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.Controls.SetChildIndex(this.button3, 0);
+            this.Controls.SetChildIndex(this.button4, 0);
+            this.Controls.SetChildIndex(this.button5, 0);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tree;
+        private System.Windows.Forms.RichTextBox codeRichTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ IDERedDot/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Bunifu.Utils;
+
+namespace IDERedDot
+{
+    public partial class Form1 : BunifuForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void codeRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // keywords e funções
+            string keywords = @"\b(public|private|partial|static|namespace|class|using|void|foreach|in|import|as|from|canvas|place)\b";
+            MatchCollection keywordsMatches = Regex.Matches(input: codeRichTextBox.Text, pattern: keywords);
+
+            // tipos e classes 
+            string type = @"\b(Console)\b";
+            MatchCollection typeMatches = Regex.Matches(input: codeRichTextBox.Text, pattern: type);
+
+            // comentarios
+            string comments = @"(\/\/.*?$|\/\*.*?\*\/|\*#)";
+            MatchCollection commentsMatches = Regex.Matches(input: codeRichTextBox.Text, pattern: comments, RegexOptions.Multiline);
+
+            // strings
+            string String = "\".+?\"";
+            MatchCollection StringMatches = Regex.Matches(input: codeRichTextBox.Text, pattern: String);
+
+            // salvando posição
+            int originalIndex = codeRichTextBox.SelectionStart;
+            int originalLength = codeRichTextBox.SelectionLength;
+            Color originalColor = Color.LightGray;
+
+            tree.Focus();
+
+            codeRichTextBox.SelectionStart = 0;
+            codeRichTextBox.SelectionLength = codeRichTextBox.Text.Length;
+            codeRichTextBox.SelectionColor = originalColor;
+
+            foreach (Match m in keywordsMatches)
+            {
+                codeRichTextBox.SelectionStart = m.Index;
+                codeRichTextBox.SelectionLength = m.Length;
+                codeRichTextBox.SelectionColor = this.HeaderBackColor;
+            }
+            foreach (Match m in typeMatches)
+            {
+                codeRichTextBox.SelectionStart = m.Index;
+                codeRichTextBox.SelectionLength = m.Length;
+                codeRichTextBox.SelectionColor = Color.DarkCyan;
+            }
+            foreach (Match m in commentsMatches)
+            {
+                codeRichTextBox.SelectionStart = m.Index;
+                codeRichTextBox.SelectionLength = m.Length;
+                codeRichTextBox.SelectionColor = Color.Green;
+            }
+            foreach (Match m in StringMatches)
+            {
+                codeRichTextBox.SelectionStart = m.Index;
+                codeRichTextBox.SelectionLength = m.Length;
+                codeRichTextBox.SelectionColor = Color.Crimson;
+            }
+
+            codeRichTextBox.SelectionStart = originalIndex;
+            codeRichTextBox.SelectionLength = originalLength;
+            codeRichTextBox.SelectionColor = originalColor;
+
+            codeRichTextBox.Focus();
+
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Show(MousePosition);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            Cursor.Current = Cursors.WaitCursor;
+            tree.Nodes.Clear();
+            foreach (var item in Directory.GetDirectories(folderBrowserDialog1.SelectedPath))
+            {
+                DirectoryInfo di = new DirectoryInfo(item);
+                var node = tree.Nodes.Add(di.Name, di.Name,0);
+                node.Tag = di;
+            }
+            foreach (var item in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+            {
+                FileInfo di = new FileInfo(item);
+                var node = tree.Nodes.Add(di.Name, di.Name, 1);
+                node.Tag = di;
+            }
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tree_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Node.Tag == null)
+            {
+                //return
+            }
+            else if (e.Node.Tag.GetType() == typeof(DirectoryInfo))
+            {
+                e.Node.Nodes.Clear();
+                foreach (var item in Directory.GetDirectories(((DirectoryInfo)e.Node.Tag).FullName))
+                {
+                    {
+                        DirectoryInfo di = new DirectoryInfo(item);
+                        var node = e.Node.Nodes.Add(di.Name, di.Name, 0,0);
+                        node.Tag = di;
+                    }
+                }
+                foreach (var item in Directory.GetFiles(((DirectoryInfo)e.Node.Tag).FullName))
+                {
+                    FileInfo di = new FileInfo(item);
+                    var node = e.Node.Nodes.Add(di.Name, di.Name, 1,1);
+                    node.Tag = di;
+                }
+            }
+            else
+            {
+                codeRichTextBox.Text = File.ReadAllText(((FileInfo)e.Node.Tag).FullName);
+            }
+
+
+
+            }
+    }
+}
